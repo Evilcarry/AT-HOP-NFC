ec47f86c81c492d6f4eff81559ea0a15dfcb9985
Evilcarry <benjhithaxx@gmail.com>
2022-05-10 15:54:19 +1200

    Added the current project to the repository
    Here I am adding the two folders, one for all the .kt files and one for the .xml files

---
+++ Kotlin(.kt)/MainScreen.kt
@@ -0,0 +1,43 @@
+package com.example.athopnfc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.example.athopnfc.fragments.TabPageAdapter
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.navigation_layout.*
+
+class MainScreen : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.navigation_layout)
+        setUpTabBar()
+    }
+
+    //This function is in charge of setting up a bar at the bottom of the screen in order to allow the user interaction.
+    private fun setUpTabBar()
+    {
+        val adapter = TabPageAdapter(this, tabLayout.tabCount)
+        viewPager.adapter = adapter
+
+        viewPager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback()
+        {
+            override fun onPageSelected(position: Int) {
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+        })
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener
+        {
+            override fun onTabSelected(tab: TabLayout.Tab)
+            {
+                viewPager.currentItem = tab.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
+    }
+}
